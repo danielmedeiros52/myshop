35f187ba247367f26f233f41b18d9498f3c52d7c
Daniel Brito <dlmf@gft.com>
2022-09-10 00:04:42 -0300

    removing non used archives

--- app/src/_metronic/layout/components/header/Topbar.tsx
+++ app/src/_metronic/layout/components/header/Topbar.tsx
@@ -29,7 +29,7 @@ const Topbar: FC = () => {
           data-kt-menu-placement='bottom-end'
           data-kt-menu-flip='bottom'
         >
-          <img src={toAbsoluteUrl('/media/avatars/300-1.jpg')} alt='metronic' />
+          <img src={toAbsoluteUrl('/media/avatars/300-3.jpg')} alt='metronic' />
         </div>
         <HeaderUserMenu />
         {/* end::Toggle */}
